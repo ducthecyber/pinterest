5aa56be74fca841750bfb6d8b37b0a6f366a2a61
ducthe <phuongduducthe@gmail.com>
2022-12-22 02:04:22 +0700

    upload ảnh lỗi,quản lý ảnh page

--- docker_pinterest/src/routes/homeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const homeRoute = express.Router();
-const {photoList, photoName} = require("../controllers/homeController");
-
-//lấy danh sách ảnh
-homeRoute.get("/photoList",photoList);
-homeRoute.get("/photoName",photoName);
-
-module.exports = homeRoute;
--- docker_pinterest/src/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRoute = express.Router();
-const {createUser, login} = require("../controllers/userController");
-
-//SIGNUP
-userRoute.post("/signUp",createUser);
-userRoute.get("/login",login);
-module.exports = userRoute;
